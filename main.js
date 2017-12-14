befbbd72022ec347056a594dbd43de13ed98812d
Ikerstreamer <ikerdeaguirre@hotmail.com>
2017-12-14 14:06:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var power = 0;
 var StoryCount = 0;
-var StoryText = ["You find yourself in a  dark room with a broken electrical box...", "You open the electrical box to find disconnected wires...", "You connect one set of wires, then you wait...","The light in the room starts to flicker...","The light has sablized, u can see now!","After looking around for a while u find a shelf standing in the far corner of the room, it is dimly lit...","You find some useless scrap and spare parts, you decide to go look somewhere else. But then just before you leave you spot a battery on the top self "]
+var StoryText = ["You find yourself in a  dark room with a broken electrical box...", "You open the electrical box to find disconnected wires...", "You connect one set of wires, then you wait...","The light in the room starts to flicker...","The light has sablized, u can see now!","After looking around for a while u find a shelf standing in the far corner of the room, it is dimly lit...","You find some useless scrap and spare parts, you decide to go look somewhere else. But then just before you leave you spot a battery on the top self...","You pick the battery, it looks a lot bigger now that you've gotten a better look at it..."];
 var weld = 0;
 var newWire = false;
 var wireTotal = 0;
@@ -22,12 +22,14 @@ function Story()
 	if(StoryCount==3 && power>=10)StoryCount++;
 	if(StoryCount==4)document.getElementById("StoryText").innerHTML="Look Around";
 	if(StoryCount==5)document.getElementById("StoryText").innerHTML="Search the Shelf";
+	if(StoryCount==6)document.getElementById("StoryText").innerHTML="Grab the Battery";
+	document.getElementById("StoryLog").innerHTML = StoryText[StoryCount];
 }
 
 function UpdateWires()
 {
-			document.getElementById("ConnectBtn").onclick=function(){
-				if(wire<wireTotal)newWire=true;
+	document.getElementById("ConnectBtn").onclick=function(){
+		if(wire<wireTotal)newWire=true;
 		}
 	if(newWire)weld+=0.05;
 	if(weld>=1)
@@ -63,7 +65,6 @@ window.setInterval(function() {
 		if(StoryCount<4)document.getElementById("StoryBtn").style.display = "none";else document.getElementById("StoryBtn").style.display = "inline";
 	}
 	document.getElementById("wireCount").innerHTML = wire+"/"+wireTotal;
-	document.getElementById("StoryLog").innerHTML = StoryText[StoryCount];
 	Story();
 	UpdatePower();
 	UpdateWires();
